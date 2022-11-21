484f61d4ffebf36a6f8a1733ff80a036e62ac7f0
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-11-21 09:45:23 +0200

    completed week3 task 1

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,24 @@
+
+
+
+  #response will have:
+    #name
+    #nationality 
+    #assists
+    #goals
+    #penalties
+    #team
+    #games
+    
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, assists, goals, penalties, team, games):
         self.name = name
+        self.nationality = nationality
+        self.assists = assists
+        self.goals = goals
+        self.penalties = penalties
+        self.team = team
+        self.games = games
     
     def __str__(self):
-        return self.name
+        return self.name + ' team ' + self.team + ' goals ' + str(self.goals) + ' assists ' + str(self.assists)
