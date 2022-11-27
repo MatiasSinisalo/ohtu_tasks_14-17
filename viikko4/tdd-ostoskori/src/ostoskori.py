f3baa4bb789a2f082e64878d7cb07821749d66ee
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-11-27 15:44:19 +0200

    ostoskori saves buy objects to a list when adding a new item and gives the length of the list with tavaroita_korissa

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,11 +3,12 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
+        self.kori = [] 
         pass
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return 0;
+        return len(self.kori)
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
@@ -18,6 +19,8 @@ class Ostoskori:
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
+        ostos = Ostos(lisattava)
+        self.kori.append(ostos)
         pass
 
     def poista_tuote(self, poistettava: Tuote):
