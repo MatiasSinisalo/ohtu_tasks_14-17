2edbebda4c40d750a9518fea07a3eb2822bfbf66
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-11-27 16:10:56 +0200

    added test for right len of the list returned by ostokset() when there are two items in the shopping cart

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -58,6 +58,13 @@ class TestOstoskori(unittest.TestCase):
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
-
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuote.nimi(), "Maito")
+    
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosta(self):
+        maito = Tuote("Maito", 3)
+        voi = Tuote("Voi", 1)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(voi)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
