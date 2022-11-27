7e2091cfaaab600313df84369b9f56d0aeafe641
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-11-27 16:41:49 +0200

    tested that the ostokset() list returns one buy object with correct name when there are two identical items in the shopping cart

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -87,4 +87,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(ostokset[0].lukumaara(), 2)
+        self.assertEqual(ostokset[0].tuote.nimi(), "Maito")
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_on_yksi_ostos_jossa_on_oikea_nimi(self):
+        maito = Tuote("Maito", 3)
+       
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(ostokset[0].tuote.nimi(), "Maito")
         
