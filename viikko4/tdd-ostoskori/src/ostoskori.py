320975ef351337cb601c7f17cb116c0935f142dc
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-11-27 16:34:45 +0200

    Ostoskori class now stores one buy object for each item type, adding multiple items of the same item doesnt create new buy objects

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,25 +3,29 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.kori = [] 
+        self.kori = {}
         pass
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return len(self.kori)
+        return sum(ostos.lukumaara() for ostos in self.kori.values())
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return sum(ostos.tuote.hinta() for ostos in self.kori)
+        return sum(ostos.hinta() for ostos in self.kori.values())
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        ostos = Ostos(lisattava)
-        self.kori.append(ostos)
-        pass
+        if(lisattava.nimi() not in self.kori):
+            ostos = Ostos(lisattava)
+            self.kori[ostos.tuote.nimi()] = ostos
+        else:
+            self.kori[lisattava.nimi()].muuta_lukumaaraa(1)
+
+
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -32,7 +36,7 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        return self.kori
+        return list(self.kori.values())
         
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
